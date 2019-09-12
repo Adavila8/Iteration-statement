3ccd8c3d8f50be1d2a1e853c16682ab1c323ca2d
gferguson520 <gferguson520@users.noreply.github.com>
2019-09-12 19:23:40 -0400

    new comment

--- Iteration statement/Program.cs
+++ Iteration statement/Program.cs
@@ -10,6 +10,30 @@ namespace Iteration_statement
     {
         static void Main(string[] args)
         {
+            //a write line statement for integer values from 1-100
+            Console.WriteLine("Enter a integer value between 1 and 100.");
+            try
+            {
+                //creating a sting to hold the variables through the code.
+                string input = Console.ReadLine();
+                int new_input = int.Parse(input);
+
+                if ((new_input >= 1) && (new_input <= 100))
+                    for (int i = 1; i < new_input + 1; i++)
+                        //Having the system write a line and including the variable.
+                        Console.WriteLine("You have entered " + i + " This is the current integer value in the loop: " + i);
+
+                Console.WriteLine("Please press Enter to end this program.");
+                Console.ReadLine();
+            }
+            // a catch for errors
+            catch
+            {
+                Console.WriteLine("An Error has occured");
+            }
+
         }
+
     }
 }
+
